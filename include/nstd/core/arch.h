07a6be91de32a4643c32bc0c993549ee38dcba71
Nyantastic UwU <NyantasticUwU@gmail.com>
2021-09-01 02:34:07 -0500

    Added NSTD_CORE_ARCH_PTR_SIZE.

---
+++ include/nstd/core/arch.h
@@ -0,0 +1,15 @@
+#ifndef NSTD_CORE_ARCH_H_INCLUDED
+#define NSTD_CORE_ARCH_H_INCLUDED
+#include "def.h"
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/// Size (in bytes) of a pointer.
+extern const NSTDCSize NSTD_CORE_ARCH_PTR_SIZE;
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/core/arch.cpp
@@ -0,0 +1,7 @@
+#include <nstd/core/arch.h>
+
+extern "C"
+{
+    /// Size (in bytes) of a pointer.
+    constexpr const NSTDCSize NSTD_CORE_ARCH_PTR_SIZE{sizeof(void *)};
+}
