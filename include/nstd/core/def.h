5510916f818bab223562302209dcba894ee1180b
Nyantastic UwU <NyantasticUwU@gmail.com>
2021-09-01 18:59:53 -0500

    Added NSTDC_NULL for C++.

--- include/nstd/core/def.h
+++ include/nstd/core/def.h
@@ -1,6 +1,10 @@
 #ifndef NSTD_CORE_DEF_H_INCLUDED
 #define NSTD_CORE_DEF_H_INCLUDED
+#ifndef __cplusplus
 #define NSTDC_NULL 0
+#else
+#define NSTDC_NULL nullptr
+#endif
 
 /// Represents a size of any type, such as a memory block.
 typedef unsigned long NSTDCSize;
