f856139b101b200ede47aa2f59d389e2ae12ca98
Nyantastic UwU <NyantasticUwU@gmail.com>
2021-09-15 03:38:12 -0500

    Added more helpful fns to the env module.

--- include/nstd/std/env.h
+++ include/nstd/std/env.h
@@ -21,10 +21,22 @@ NSTDAPI char *nstd_std_env_path_to_exe(int *errc);
 /// Returns: `char *path` - The path of the working directory.
 NSTDAPI char *nstd_std_env_current_dir(int *errc);
 
-/// Frees memory allocated by `nstd_std_env_path_to_exe` or `nstd_std_env_current_dir`.
+/// Returns the path of a temporary directory.
+/// Use `nstd_std_env_free_path` to free memory allocated by this function.
+/// Returns: `char *path` - The path of the temp dir.
+NSTDAPI char *nstd_std_env_temp_dir();
+
+/// Frees memory allocated by `nstd_std_env_path_to_exe`,  `nstd_std_env_current_dir` or
+/// `nstd_std_env_temp_dir`.
 /// Parameters:
 ///     `char **path` - String from `nstd_std_env_path_to_exe` or `nstd_std_env_current_dir`.
 NSTDAPI void nstd_std_env_free_path(char **path);
+
+/// Sets the current working directory.
+/// Parameters:
+///     `const char *const path` - The new working directory.
+/// Returns: `int errc` - Nonzero on error.
+NSTDAPI int nstd_std_env_set_current_dir(const char *const path);
 
 /// Returns an array of strings that contain the cmd args that the program was started with.
 /// Parameters:
--- src/std/env/src/lib.rs
+++ src/std/env/src/lib.rs
@@ -1,6 +1,6 @@
 use std::{
     env,
-    ffi::CString,
+    ffi::{CStr, CString},
     os::raw::{c_char, c_int},
     ptr,
 };
@@ -30,13 +30,40 @@ macro_rules! nstd_path_fns {
 nstd_path_fns!(nstd_std_env_path_to_exe, current_exe);
 nstd_path_fns!(nstd_std_env_current_dir, current_dir);
 
-/// Frees memory allocated by `nstd_std_env_path_to_exe` or `nstd_std_env_current_dir`.
+/// Returns the path of a temporary directory.
+/// Use `nstd_std_env_free_path` to free memory allocated by this function.
+/// Returns: `char *path` - The path of the temp dir.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_env_temp_dir() -> *mut c_char {
+    match env::temp_dir().into_os_string().into_string() {
+        Ok(path) => CString::from_vec_unchecked(path.into_bytes()).into_raw(),
+        _ => ptr::null_mut(),
+    }
+}
+
+/// Frees memory allocated by `nstd_std_env_path_to_exe`,  `nstd_std_env_current_dir` or
+/// `nstd_std_env_temp_dir`.
 /// Parameters:
 ///     `char **path` - String from `nstd_std_env_path_to_exe` or `nstd_std_env_current_dir`.
 #[no_mangle]
 pub unsafe extern "C" fn nstd_std_env_free_path(path: *mut *mut c_char) {
     CString::from_raw(*path);
     *path = ptr::null_mut();
+}
+
+/// Sets the current working directory.
+/// Parameters:
+///     `const char *const path` - The new working directory.
+/// Returns: `int errc` - Nonzero on error.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_env_set_current_dir(path: *const c_char) -> c_int {
+    match CStr::from_ptr(path).to_str() {
+        Ok(path) => match env::set_current_dir(path) {
+            Ok(_) => 0,
+            _ => 1,
+        },
+        _ => 1,
+    }
 }
 
 /// Returns an array of strings that contain the cmd args that the program was started with.
