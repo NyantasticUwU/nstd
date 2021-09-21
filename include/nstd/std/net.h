70c8a201a670ce31385a34839a3217acc636e24b
Nyantastic UwU <NyantasticUwU@gmail.com>
2021-09-21 06:27:35 -0500

    Added UDP networking.

--- include/nstd/std/net.h
+++ include/nstd/std/net.h
@@ -13,6 +13,9 @@ typedef void *NSTDTCPServer;
 /// Represents a TCP stream.
 typedef void *NSTDTCPStream;
 
+/// Represents a UDP socket.
+typedef void *NSTDUDPSocket;
+
 /// Creates a TCP server bound to the given address. Call `nstd_std_net_tcp_server_close` to free
 /// memory allocated by this function and close the server.
 /// Parameters:
@@ -60,6 +63,82 @@ NSTDAPI int nstd_std_net_tcp_stream_write(
 /// Parameters:
 ///     `NSTDTCPStream *stream` - Pointer to the TCP stream.
 NSTDAPI void nstd_std_net_tcp_stream_close(NSTDTCPStream *stream);
+
+/// Creates a UDP socket bound to the given address. Call `nstd_std_net_udp_socket_close` to free
+/// memory allocated by this function and close the socket.
+/// Parameters:
+///     `const char *const addr` - The address to listen on, formatted as "IP:Port".
+/// Returns: `NSTDUDPSocket socket` - The UDP socket, null on error.
+NSTDAPI NSTDUDPSocket nstd_std_net_udp_socket_bind(const char *const addr);
+
+/// Connects a UDP socket to a remote address.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The socket to connect.
+///     `const char *const addr` - The remote address to connect to.
+/// Returns: `int errc` - Nonzero on error.
+NSTDAPI int nstd_std_net_udp_socket_connect(NSTDUDPSocket socket, const char *const addr);
+
+/// Receives bytes sent from the connected address.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The socket to receive bytes on.
+///     `const NSTDSize num` - Number of bytes to receive.
+///     `NSTDSize *size` - Returns as actual number of bytes received.
+/// Returns: `NSTDByte *bytes` - The bytes received.
+NSTDAPI NSTDByte *nstd_std_net_udp_socket_receive(
+    NSTDUDPSocket socket,
+    const NSTDSize num,
+    NSTDSize *size);
+
+/// Receives bytes sent to a UDP socket.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The socket to receive bytes from.
+///     `const NSTDSize num` - Number of bytes to receive.
+///     `NSTDSize *size` - Returns as actual number of bytes received.
+///     `char **ip` - Returns as the socket IP address the bytes came from.
+/// Returns: `NSTDByte *bytes` - The bytes received.
+NSTDAPI NSTDByte *nstd_std_net_udp_socket_receive_from(
+    NSTDUDPSocket socket,
+    const NSTDSize num,
+    NSTDSize *size,
+    char **ip);
+
+/// Sends bytes from a UDP socket to it's connected address.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The UDP socket.
+///     `const NSTDByte *const bytes` - The bytes to send.
+///     `const NSTDSize num` - Number of bytes to send.
+///     `NSTDSize *size` - Returns as number of bytes actually sent.
+/// Returns: `int errc` - Nonzero on error.
+NSTDAPI int nstd_std_net_udp_socket_send(
+    NSTDUDPSocket socket,
+    const NSTDByte *const bytes,
+    const NSTDSize num,
+    NSTDSize *size);
+
+/// Sends bytes from a UDP socket to another.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The UDP socket.
+///     `const char *const addr` - The address to send the bytes to.
+///     `const NSTDByte *const bytes` - The bytes to send.
+///     `const NSTDSize num` - Number of bytes to send.
+///     `NSTDSize *size` - Returns as number of bytes actually sent.
+/// Returns: `int errc` - Nonzero on error.
+NSTDAPI int nstd_std_net_udp_socket_send_to(
+    NSTDUDPSocket socket,
+    const char *const addr,
+    const NSTDByte *const bytes,
+    const NSTDSize num,
+    NSTDSize *size);
+
+/// Closes and frees memory of a UDP socket.
+/// Parameters:
+///     `NSTDUDPSocket *socket` - Pointer to the UDP socket.
+NSTDAPI void nstd_std_net_udp_socket_close(NSTDUDPSocket *socket);
+
+/// Deallocates memory where an IP address string is allocated.
+/// Parameters:
+///     `char **ip` - The IP address.
+NSTDAPI void nstd_std_net_free_ip(char **ip);
 
 /// Frees bytes allocated by any of the `nstd_std_net_*` functions.
 /// parameters:
--- src/std/net/src/lib.rs
+++ src/std/net/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
-    ffi::CStr,
+    ffi::{CStr, CString},
     io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
+    net::{TcpListener, TcpStream, UdpSocket},
     os::raw::{c_char, c_int, c_uchar, c_void},
     ptr, slice,
 };
@@ -11,6 +11,9 @@ type NSTDTCPServer = *mut c_void;
 
 /// Represents a TCP stream.
 type NSTDTCPStream = *mut c_void;
+
+/// Represents a UDP socket.
+type NSTDUDPSocket = *mut c_void;
 
 /// Creates a TCP server bound to the given address. Call `nstd_std_net_tcp_server_close` to free
 /// memory allocated by this function and close the server.
@@ -114,6 +117,166 @@ pub unsafe extern "C" fn nstd_std_net_tcp_stream_write(
 pub unsafe extern "C" fn nstd_std_net_tcp_stream_close(stream: *mut NSTDTCPStream) {
     Box::from_raw(*stream as *mut TcpStream);
     *stream = ptr::null_mut();
+}
+
+/// Creates a UDP socket bound to the given address. Call `nstd_std_net_udp_socket_close` to free
+/// memory allocated by this function and close the socket.
+/// Parameters:
+///     `const char *const addr` - The address to listen on, formatted as "IP:Port".
+/// Returns: `NSTDUDPSocket socket` - The UDP socket, null on error.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_net_udp_socket_bind(addr: *const c_char) -> NSTDUDPSocket {
+    match CStr::from_ptr(addr).to_str() {
+        Ok(addr) => match UdpSocket::bind(addr) {
+            Ok(socket) => Box::into_raw(Box::new(socket)) as NSTDUDPSocket,
+            _ => ptr::null_mut(),
+        },
+        _ => ptr::null_mut(),
+    }
+}
+
+/// Connects a UDP socket to a remote address.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The socket to connect.
+///     `const char *const addr` - The remote address to connect to.
+/// Returns: `int errc` - Nonzero on error.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_net_udp_socket_connect(
+    socket: NSTDUDPSocket,
+    addr: *const c_char,
+) -> c_int {
+    let socket = &*(socket as *mut UdpSocket);
+    match CStr::from_ptr(addr).to_str() {
+        Ok(addr) => match socket.connect(addr) {
+            Ok(_) => 0,
+            _ => 1,
+        },
+        _ => 1,
+    }
+}
+
+/// Receives bytes sent from the connected address.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The socket to receive bytes on.
+///     `const NSTDSize num` - Number of bytes to receive.
+///     `NSTDSize *size` - Returns as actual number of bytes received.
+/// Returns: `NSTDByte *bytes` - The bytes received.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_net_udp_socket_receive(
+    socket: NSTDUDPSocket,
+    num: usize,
+    size: *mut usize,
+) -> *mut c_uchar {
+    let socket = &*(socket as *mut UdpSocket);
+    let mut buf = Vec::new();
+    buf.resize(num, 0);
+    match socket.recv(&mut buf) {
+        Ok(recv_size) => {
+            *size = recv_size;
+            Box::into_raw(buf.into_boxed_slice()) as *mut c_uchar
+        }
+        _ => ptr::null_mut(),
+    }
+}
+
+/// Receives bytes sent to a UDP socket.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The socket to receive bytes from.
+///     `const NSTDSize num` - Number of bytes to receive.
+///     `NSTDSize *size` - Returns as actual number of bytes received.
+///     `char **ip` - Returns as the socket IP address the bytes came from.
+/// Returns: `NSTDByte *bytes` - The bytes received.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_net_udp_socket_receive_from(
+    socket: NSTDUDPSocket,
+    num: usize,
+    size: *mut usize,
+    ip: *mut *mut c_char,
+) -> *mut c_uchar {
+    let socket = &*(socket as *mut UdpSocket);
+    let mut buf = Vec::new();
+    buf.resize(num, 0);
+    match socket.recv_from(&mut buf) {
+        Ok((recv_size, recv_ip)) => {
+            *size = recv_size;
+            let mut ipv = recv_ip.to_string().into_bytes();
+            ipv.push(0);
+            *ip = CString::from_vec_unchecked(ipv).into_raw();
+            Box::into_raw(buf.into_boxed_slice()) as *mut c_uchar
+        }
+        _ => ptr::null_mut(),
+    }
+}
+
+/// Sends bytes from a UDP socket to another.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The UDP socket.
+///     `const NSTDByte *const bytes` - The bytes to send.
+///     `const NSTDSize num` - Number of bytes to send.
+///     `NSTDSize *size` - Returns as number of bytes actually sent.
+/// Returns: `int errc` - Nonzero on error.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_net_udp_socket_send(
+    socket: NSTDUDPSocket,
+    bytes: *const c_uchar,
+    num: usize,
+    size: *mut usize,
+) -> c_int {
+    let socket = &*(socket as *mut UdpSocket);
+    match socket.send(slice::from_raw_parts(bytes, num)) {
+        Ok(sent) => {
+            *size = sent;
+            0
+        }
+        _ => 1,
+    }
+}
+
+/// Sends bytes from a UDP socket to another.
+/// Parameters:
+///     `NSTDUDPSocket socket` - The UDP socket.
+///     `const char *const addr` - The address to send the bytes to.
+///     `const NSTDByte *const bytes` - The bytes to send.
+///     `const NSTDSize num` - Number of bytes to send.
+///     `NSTDSize *size` - Returns as number of bytes actually sent.
+/// Returns: `int errc` - Nonzero on error.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_net_udp_socket_send_to(
+    socket: NSTDUDPSocket,
+    addr: *const c_char,
+    bytes: *const c_uchar,
+    num: usize,
+    size: *mut usize,
+) -> c_int {
+    let socket = &*(socket as *mut UdpSocket);
+    match CStr::from_ptr(addr).to_str() {
+        Ok(addr) => match socket.send_to(slice::from_raw_parts(bytes, num), addr) {
+            Ok(sent) => {
+                *size = sent;
+                0
+            }
+            _ => 1,
+        },
+        _ => 1,
+    }
+}
+
+/// Closes and frees memory of a UDP socket.
+/// Parameters:
+///     `NSTDUDPSocket *socket` - Pointer to the UDP socket.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_net_udp_socket_close(socket: *mut NSTDUDPSocket) {
+    Box::from_raw(*socket as *mut UdpSocket);
+    *socket = ptr::null_mut();
+}
+
+/// Deallocates memory where an IP address string is allocated.
+/// Parameters:
+///     `char **ip` - The IP address.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_net_free_ip(ip: *mut *mut c_char) {
+    CString::from_raw(*ip);
+    *ip = ptr::null_mut();
 }
 
 /// Frees bytes allocated by any of the `nstd_std_net_*` functions.
