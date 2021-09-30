a482c94f4f8fd3b6204e0e042df61025ee3689a6
Nyantastic UwU <NyantasticUwU@gmail.com>
2021-09-30 08:25:31 -0500

    Basic window manipulation.

--- include/nstd/std/gui.h
+++ include/nstd/std/gui.h
@@ -16,12 +16,54 @@ typedef void *NSTDWindow;
 /// Returns: `NSTDWindow window` - The new window, null on error.
 NSTDAPI NSTDWindow nstd_std_gui_window_create(NSTDEventLoop event_loop);
 
+/// Gets a window's scale factor.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+/// Returns: `double factor` - The scale factor of the window.
+NSTDAPI double nstd_std_gui_window_get_scale_factor(NSTDWindow window);
+
 /// Sets a window's title.
 /// Parameters:
 ///     `NSTDWindow window` - The window.
 ///     `const char *const title` - The new window title.
 /// Returns: `int errc` - Nonzero on error.
 NSTDAPI int nstd_std_gui_window_set_title(NSTDWindow window, const char *const title);
+
+/// Sets a window's visibility.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int visible` - Whether to show or hide the window.
+NSTDAPI void nstd_std_gui_window_set_visible(NSTDWindow window, const int visible);
+
+/// Sets whether the window is resizable or not.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int resizable` - Whether the window should be resizable or not.
+NSTDAPI void nstd_std_gui_window_set_resizable(NSTDWindow window, const int resizable);
+
+/// Sets the window's minimization mode.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int minimized` - Whether the window should be minimized or not.
+NSTDAPI void nstd_std_gui_window_set_minimized(NSTDWindow window, const int minimized);
+
+/// Sets the window's maximization mode.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int maximized` - Whether the window should be maximized or not.
+NSTDAPI void nstd_std_gui_window_set_maximized(NSTDWindow window, const int maximized);
+
+/// Checks if the window is maximized.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+/// Returns: `int maximized` - Nonzero if the window is maximized.
+NSTDAPI int nstd_std_gui_window_is_maximized(NSTDWindow window);
+
+/// Turn window decorations on or off.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int decorations` - Whether to allow window decorations or not.
+NSTDAPI void nstd_std_gui_window_set_decorations(NSTDWindow window, const int decorations);
 
 #ifdef __cplusplus
 }
--- src/std/gui/src/lib.rs
+++ src/std/gui/src/lib.rs
@@ -1,7 +1,7 @@
 use nstd_events::NSTDEventLoop;
 use std::{
     ffi::CStr,
-    os::raw::{c_char, c_int, c_void},
+    os::raw::{c_char, c_double, c_int, c_void},
     ptr,
 };
 use winit::{event_loop::EventLoop, window::Window};
@@ -22,6 +22,16 @@ pub unsafe extern "C" fn nstd_std_gui_window_create(event_loop: NSTDEventLoop) -
     }
 }
 
+/// Gets a window's scale factor.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+/// Returns: `double factor` - The scale factor of the window.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_gui_window_get_scale_factor(window: NSTDWindow) -> c_double {
+    let window = &*(window as *mut Window);
+    window.scale_factor()
+}
+
 /// Sets a window's title.
 /// Parameters:
 ///     `NSTDWindow window` - The window.
@@ -40,4 +50,67 @@ pub unsafe extern "C" fn nstd_std_gui_window_set_title(
         }
         _ => 1,
     }
+}
+
+/// Sets a window's visibility.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int visible` - Whether to show or hide the window.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_gui_window_set_visible(window: NSTDWindow, visible: c_int) {
+    let window = &*(window as *mut Window);
+    window.set_visible(visible != 0);
+}
+
+/// Sets whether the window is resizable or not.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int resizable` - Whether the window should be resizable or not.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_gui_window_set_resizable(window: NSTDWindow, resizable: c_int) {
+    let window = &*(window as *mut Window);
+    window.set_resizable(resizable != 0);
+}
+
+/// Sets the window's minimization mode.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int minimized` - Whether the window should be minimized or not.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_gui_window_set_minimized(window: NSTDWindow, minimized: c_int) {
+    let window = &*(window as *mut Window);
+    window.set_minimized(minimized != 0);
+}
+
+/// Sets the window's maximization mode.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int maximized` - Whether the window should be maximized or not.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_gui_window_set_maximized(window: NSTDWindow, maximized: c_int) {
+    let window = &*(window as *mut Window);
+    window.set_maximized(maximized != 0);
+}
+
+/// Checks if the window is maximized.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+/// Returns: `int maximized` - Nonzero if the window is maximized.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_gui_window_is_maximized(window: NSTDWindow) -> c_int {
+    let window = &*(window as *mut Window);
+    window.is_maximized() as c_int
+}
+
+/// Turn window decorations on or off.
+/// Parameters:
+///     `NSTDWindow window` - The window.
+///     `const int decorations` - Whether to allow window decorations or not.
+#[no_mangle]
+pub unsafe extern "C" fn nstd_std_gui_window_set_decorations(
+    window: NSTDWindow,
+    decorations: c_int,
+) {
+    let window = &*(window as *mut Window);
+    window.set_decorations(decorations != 0);
 }
