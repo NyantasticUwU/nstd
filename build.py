af384326400d7ecd39df9da93ef3850ace95bbcc
Nyantastic UwU <NyantasticUwU@gmail.com>
2021-10-16 19:17:04 -0500

    Extra arg options to build.py.

--- build.py
+++ build.py
@@ -7,25 +7,34 @@ MODULES = ("audio", "env", "events", "fs", "gui", "image", "io",
 
 # Main entry point of program.
 if __name__ == "__main__":
+    args = None
+    argc = len(sys.argv)
+    if argc > 1:
+        args = ""
+        for i in range(1, argc):
+            args += f"{sys.argv[i]} "
     if not os.path.exists("lib"):
         os.mkdir("lib")
     for module in MODULES:
         print(f"Building nstd_{module}...")
         os.chdir(f"src/std/{module}")
-        os.system("cargo build --release --quiet")
-        if sys.platform.startswith("win32"):
-            SRC = f"target/release/nstd_{module}.lib"
-            DEST = f"../../../lib/nstd_{module}.lib"
-            if os.path.exists(DEST):
-                os.replace(SRC, DEST)
-            else:
-                os.rename(SRC, DEST)
-        elif sys.platform.startswith("linux"):
-            SRC = f"target/release/libnstd_{module}.a"
-            DEST = f"../../../lib/libnstd_{module}.a"
-            if os.path.exists(DEST):
-                os.replace(SRC, DEST)
-            else:
-                os.rename(SRC, DEST)
+        if args == None:
+            os.system("cargo build --release --quiet")
+            if sys.platform.startswith("win32"):
+                SRC = f"target/release/nstd_{module}.lib"
+                DEST = f"../../../lib/nstd_{module}.lib"
+                if os.path.exists(DEST):
+                    os.replace(SRC, DEST)
+                else:
+                    os.rename(SRC, DEST)
+            elif sys.platform.startswith("linux"):
+                SRC = f"target/release/libnstd_{module}.a"
+                DEST = f"../../../lib/libnstd_{module}.a"
+                if os.path.exists(DEST):
+                    os.replace(SRC, DEST)
+                else:
+                    os.rename(SRC, DEST)
+        else:
+            os.system(f"cargo {args}")
         os.chdir("../../../")
         print(f"Finished nstd_{module}.")
