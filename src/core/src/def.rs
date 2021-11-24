9e44018940307f5d1af4117d7c95a47aaab070cc
Nyantastic UwU <NyantasticUwU@gmail.com>
2021-11-24 16:32:38 -0600

    impl Into<Range<*64>> for NSTD*Range.

--- src/core/src/def.rs
+++ src/core/src/def.rs
@@ -1,4 +1,4 @@
-use core::ffi::c_void;
+use core::{ffi::c_void, ops::Range};
 
 /// Returns a null pointer.
 /// Returns: `void *null` - A null pointer.
@@ -15,10 +15,22 @@ pub struct NSTDIRange {
     pub start: i64,
     pub end: i64,
 }
+impl Into<Range<i64>> for NSTDIRange {
+    #[inline]
+    fn into(self) -> Range<i64> {
+        self.start..self.end
+    }
+}
 /// Represents an unsigned range.
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Default, Hash)]
 pub struct NSTDURange {
     pub start: u64,
     pub end: u64,
+}
+impl Into<Range<u64>> for NSTDURange {
+    #[inline]
+    fn into(self) -> Range<u64> {
+        self.start..self.end
+    }
 }
