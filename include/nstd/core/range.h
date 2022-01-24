585b4f97ad1bdaf4cd57424908ac1c192bc201ad
Nyantastic UwU <NyantasticUwU@gmail.com>
2022-01-24 11:41:29 -0600

    Added `NSTD*Range*` types.

--- include/nstd/core/range.h
+++ include/nstd/core/range.h
@@ -6,6 +6,74 @@ extern "C"
 {
 #endif
 
+/// Represents an 8-bit signed range.
+typedef struct
+{
+    /// Start of the range (included).
+    NSTDInt8 start;
+    /// End of the range (exluded).
+    NSTDInt8 end;
+} NSTDIRange8;
+/// Represents an 8-bit unsigned range.
+typedef struct
+{
+    /// Start of the range (included).
+    NSTDUInt8 start;
+    /// End of the range (exluded).
+    NSTDUInt8 end;
+} NSTDURange8;
+
+/// Represents a 16-bit signed range.
+typedef struct
+{
+    /// Start of the range (included).
+    NSTDInt16 start;
+    /// End of the range (exluded).
+    NSTDInt16 end;
+} NSTDIRange16;
+/// Represents a 16-bit unsigned range.
+typedef struct
+{
+    /// Start of the range (included).
+    NSTDUInt16 start;
+    /// End of the range (exluded).
+    NSTDUInt16 end;
+} NSTDURange16;
+
+/// Represents a 32-bit signed range.
+typedef struct
+{
+    /// Start of the range (included).
+    NSTDInt32 start;
+    /// End of the range (exluded).
+    NSTDInt32 end;
+} NSTDIRange32;
+/// Represents a 32-bit unsigned range.
+typedef struct
+{
+    /// Start of the range (included).
+    NSTDUInt32 start;
+    /// End of the range (exluded).
+    NSTDUInt32 end;
+} NSTDURange32;
+
+/// Represents a 64-bit signed range.
+typedef struct
+{
+    /// Start of the range (included).
+    NSTDInt64 start;
+    /// End of the range (exluded).
+    NSTDInt64 end;
+} NSTDIRange64;
+/// Represents a 64-bit unsigned range.
+typedef struct
+{
+    /// Start of the range (included).
+    NSTDUInt64 start;
+    /// End of the range (exluded).
+    NSTDUInt64 end;
+} NSTDURange64;
+
 /// Represents a signed range.
 typedef struct
 {
--- src/core/src/range.rs
+++ src/core/src/range.rs
@@ -1,32 +1,32 @@
 use core::ops::Range;
 
-/// Represents a signed range.
-#[repr(C)]
-#[derive(Clone, Copy, Debug, Default, Hash)]
-pub struct NSTDIRange {
-    /// Start of the range (included).
-    pub start: isize,
-    /// End of the range (exluded).
-    pub end: isize,
+/// Generates the `NSTD*Range[*]` struct.
+macro_rules! nstd_gen_range_struct {
+    ($name: ident, $type: ty) => {
+        /// Represents a signed range.
+        #[repr(C)]
+        #[derive(Clone, Copy, Debug, Default, Hash)]
+        pub struct $name {
+            /// Start of the range (included).
+            pub start: $type,
+            /// End of the range (exluded).
+            pub end: $type,
+        }
+        impl Into<Range<$type>> for $name {
+            #[inline]
+            fn into(self) -> Range<$type> {
+                self.start..self.end
+            }
+        }
+    };
 }
-impl Into<Range<isize>> for NSTDIRange {
-    #[inline]
-    fn into(self) -> Range<isize> {
-        self.start..self.end
-    }
-}
-/// Represents an unsigned range.
-#[repr(C)]
-#[derive(Clone, Copy, Debug, Default, Hash)]
-pub struct NSTDURange {
-    /// Start of the range (included).
-    pub start: usize,
-    /// End of the range (exluded).
-    pub end: usize,
-}
-impl Into<Range<usize>> for NSTDURange {
-    #[inline]
-    fn into(self) -> Range<usize> {
-        self.start..self.end
-    }
-}
+nstd_gen_range_struct!(NSTDIRange8, i8);
+nstd_gen_range_struct!(NSTDURange8, u8);
+nstd_gen_range_struct!(NSTDIRange16, i16);
+nstd_gen_range_struct!(NSTDURange16, u16);
+nstd_gen_range_struct!(NSTDIRange32, i32);
+nstd_gen_range_struct!(NSTDURange32, u32);
+nstd_gen_range_struct!(NSTDIRange64, i64);
+nstd_gen_range_struct!(NSTDURange64, u64);
+nstd_gen_range_struct!(NSTDIRange, isize);
+nstd_gen_range_struct!(NSTDURange, usize);
