c9eaf6cee402d3e8b5e8f8384ee92c89fe67c5cc
Nyantastic UwU <NyantasticUwU@gmail.com>
2022-02-07 19:26:11 -0600

    Start on `nstd.os.windows.io` streams.

--- include/nstd/os/windows/io.h
+++ include/nstd/os/windows/io.h
@@ -1,10 +1,14 @@
 #ifndef NSTD_OS_WINDOWS_IO_H_INCLUDED
 #define NSTD_OS_WINDOWS_IO_H_INCLUDED
 #include "../../core/def.h"
+#include "../../core/slice.h"
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+
+/// Represents a handle to a standard IO stream.
+typedef NSTDUInt32 NSTDOSWindowsIOHandle;
 
 /// Initializes the `nstd.os.windows.io` module. This function should be called before any others.
 /// Returns: `NSTDErrorCode errc` - Nonzero on error.
@@ -21,6 +25,19 @@ NSTDAPI NSTDErrorCode nstd_os_windows_io_print(const NSTDChar *const cstr);
 ///     `const NSTDChar *const cstr` - The C string to write to stdout.
 /// Returns: `NSTDErrorCode errc` - Nonzero on error.
 NSTDAPI NSTDErrorCode nstd_os_windows_io_print_line(const NSTDChar *const cstr);
+
+/// Retrieves a handle to stdout.
+/// Returns: `NSTDOSWindowsIOHandle stdout` - The standard output stream.
+NSTDAPI NSTDOSWindowsIOHandle nstd_os_windows_io_stdout();
+
+/// Writes a buffer of `bytes` to `stream`.
+/// Parameters:
+///     `const NSTDOSWindowsIOHandle stream` - Handle to a standard IO stream.
+///     `const NSTDSlice *const bytes` - The bytes to write to the stream.
+/// Returns: `NSTDErrorCode errc` - Nonzero on error.
+NSTDAPI NSTDErrorCode nstd_os_windows_io_write(
+    const NSTDOSWindowsIOHandle stream,
+    const NSTDSlice *const bytes);
 
 #ifdef __cplusplus
 }
--- src/os/windows/io.rs
+++ src/os/windows/io.rs
@@ -1,8 +1,16 @@
-use crate::core::def::{NSTDChar, NSTDErrorCode};
+use crate::core::{
+    def::{NSTDChar, NSTDErrorCode},
+    slice::NSTDSlice,
+};
 use windows::Win32::{
     Globalization::CP_UTF8,
-    System::Console::{GetStdHandle, SetConsoleOutputCP, WriteConsoleA, STD_OUTPUT_HANDLE},
+    System::Console::{
+        GetStdHandle, SetConsoleOutputCP, WriteConsoleA, STD_HANDLE, STD_OUTPUT_HANDLE,
+    },
 };
+
+/// Represents a handle to a standard IO stream.
+pub type NSTDOSWindowsIOHandle = u32;
 
 /// Initializes the `nstd.os.windows.io` module. This function should be called before any others.
 /// Returns: `NSTDErrorCode errc` - Nonzero on error.
@@ -40,4 +48,33 @@ pub unsafe extern "C" fn nstd_os_windows_io_print_line(cstr: *const NSTDChar) ->
     let a = nstd_os_windows_io_print(cstr);
     let b = nstd_os_windows_io_print(newline.as_ptr().cast());
     a | b
+}
+
+/// Retrieves a handle to stdout.
+/// Returns: `NSTDOSWindowsIOHandle stdout` - The standard output stream.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_os_windows_io_stdout() -> NSTDOSWindowsIOHandle {
+    STD_OUTPUT_HANDLE.0
+}
+
+/// Writes a buffer of `bytes` to `stream`.
+/// Parameters:
+///     `const NSTDOSWindowsIOHandle stream` - Handle to a standard IO stream.
+///     `const NSTDSlice *const bytes` - The bytes to write to the stream.
+/// Returns: `NSTDErrorCode errc` - Nonzero on error.
+#[inline]
+#[cfg_attr(feature = "clib", no_mangle)]
+pub unsafe extern "C" fn nstd_os_windows_io_write(
+    stream: NSTDOSWindowsIOHandle,
+    bytes: &NSTDSlice,
+) -> NSTDErrorCode {
+    (WriteConsoleA(
+        GetStdHandle(STD_HANDLE(stream)),
+        bytes.ptr.raw,
+        bytes.size as u32,
+        std::ptr::null_mut(),
+        std::ptr::null_mut(),
+    )
+    .0 == 0) as NSTDErrorCode
 }
